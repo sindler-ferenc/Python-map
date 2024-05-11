b62b62a039acb6474fccaf74dedd288855088474
Sindler Ferenc <56120116+sindler-ferenc@users.noreply.github.com>
2024-05-11 18:36:35 +0200

    Init

---
+++ main.py
@@ -0,0 +1,7 @@
+# Ez egy egysoros komment.
+
+''' 
+    Többsoros kommentet írhatunk
+    3 aposztróf vagy 3 idézőjel közé írva.
+    Így szoktuk megadni általában a program vagy programrészlet leírását.
+'''
