602ca49d0f53891ce00d3c8a49a4a4db00e302b3
Sindler Ferenc <56120116+sindler-ferenc@users.noreply.github.com>
2024-10-11 18:57:43 +0200

    Update lista.py

--- lista.py
+++ lista.py
@@ -315,4 +315,21 @@ print(list(zip(diakok, eletkorok)))
 for diak, eletkor in zip(diakok, eletkorok):
     print(f'{diak}: {eletkor}')      
     
+#  Listaelemek leképezése
+
+eredeti = [11, 19, 7, -3]
+eredmeny = [x * 2 for x in eredeti]
+eredmeny_szurve = [x * 2 for x in eredeti if x > 0]
+
+
+szamok = [5, 8, 3, 11, 12]
+paros_szamok = [x for x in szamok if x % 2 == 0]
+
+
+dupla = [x * 2 for x in range(1, 10)] 
+print(dupla)
+
+# For vagy while ciklus?
   
+# Kimarad mert nem leírható
+# https://youtu.be/m3qQPvE0aSE?si=oTzfobQkbXZbzeZE 
